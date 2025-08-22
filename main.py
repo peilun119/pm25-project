35598b458e5956468ced8947bf0b785ecf5d5f6a
sghost <aa123@gmail.com>
2025-08-22 21:21:53 +0800

    新增書籍列表

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
+
+
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
 
 
 app = Flask(__name__)  # 以此檔案當作程式起始點
@@ -17,10 +36,11 @@ def get_bmi(h, w):
 @app.route("/books/id=<int:id>")
 def get_books(id=None):
     try:
-        books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+        # books = {1: "Python book", 2: "Java book", 3: "Flask book"}
 
         if id == None:  # 沒帶參數 傳回所有書籍
-            return books
+            # return books
+            return render_template("books.html", books=books)
 
         return books[id]
     except Exception as e:
@@ -39,7 +59,9 @@ def now_time():
 # 最後一定要將資料return出去才會顯示在網頁上
 @app.route("/")
 def index():
-    return "<h1>這是首頁!</h1>"
+    # return "<h1>這是首頁!</h1>"
+    timenow = now_time()
+    return render_template("index.html", time=timenow)  # 回傳網頁模板及參數 到前端網頁
 
 
 app.run(debug=True)  # 程式的尾端
